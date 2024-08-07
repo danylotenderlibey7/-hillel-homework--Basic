4f9a9972d0d7c3fc613d6d60d6ea603fdb299f62
danylo <lysukdanil678@gmail.com>
2024-08-07 15:55:23 +0300

    Added Homework 4

---
+++ Hillel_HomeWorks/4.1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Input the digit from 1 to 7");
+            int digit = Convert.ToInt32(Console.ReadLine());
+
+            switch (digit)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid digit inputed");
+                    break;
+            }
+        }
+    }
+}
---
+++ Hillel_HomeWorks/4.2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Input first number");
+            double num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input second number");
+            double num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input the operation (+, -, *, /)");
+            string operation = Console.ReadLine();
+
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine($"Result: {num1 + num2}");
+                    break;
+                case "-":
+                    Console.WriteLine($"Result: {num1 - num2}");
+                    break;
+                case "*":
+                    Console.WriteLine($"Result: {num1 * num2}");
+                    break;
+                case "/":
+                    if (num2 == 0)
+                    {
+                        Console.WriteLine("It's forbidden to devide by zero");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Result: {(num1 / num2):F2}");
+                        break;
+                    }
+                default:
+                    Console.WriteLine("Invalid symbol inputed");
+                    break;
+
+            }
+        }
+    }
+}
