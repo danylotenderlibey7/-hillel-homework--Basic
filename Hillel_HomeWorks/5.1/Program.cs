d4d35866d19b297275305ac1102aae8d29d2bc79
danylo <lysukdanil678@gmail.com>
2024-08-10 12:45:52 +0300

    Added Homework 5.1/5.2

---
+++ Hillel_HomeWorks/5.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Input first digit");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Input second digit");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+                
+                if (num2 == 0)
+                {
+                    throw new DivideByZeroException(); 
+                }
+
+                double result = num1 / num2;
+                Console.WriteLine($"Result: {(result):F2}");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("It's forbidden to devide by zero");
+            }
+        }
+    }
+}
---
+++ Hillel_HomeWorks/5.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a number:");
+            try
+            {
+                string str_number = Console.ReadLine();
+                double number = Convert.ToDouble(str_number);
+
+                Console.WriteLine($"Your number: {number}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: Inputed an invalid number.");
+            }
+        }
+    }
+}
