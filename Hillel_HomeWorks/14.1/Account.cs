c8f72ab012c5cdd3e4a775c440300450547323d5
danylo <lysukdanil678@gmail.com>
2024-09-17 13:14:15 +0300

    Added 14.1 Homework

---
+++ Hillel_HomeWorks/14.1/Account.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14._1
+{
+    class Account
+    {
+        private string name;
+        private double sum;
+
+        public Account(string name, double balance)
+        {
+            this.name = name;
+            if (balance >= 0)
+            {
+                this.sum = balance;
+            }
+            else
+            {
+                //this.sum = 0;
+                Console.WriteLine("Balance cannot be negative");
+            }
+        }
+
+        public void PutMoney(double amount)
+        {
+            if (amount > 0)
+            {
+                sum += amount;
+                Console.WriteLine($"Added {amount}$");
+            }
+            else
+            {
+                Console.WriteLine("Amount must be positive");
+            }
+        }
+
+        public void WithdrawMoney(double amount)
+        {
+            if (amount > 0 && amount <= sum)
+            {
+                sum -= amount;
+                Console.WriteLine($"Withdrew {amount}$");
+            }
+            else if (amount > sum)
+            {
+                Console.WriteLine("Not enough balance for the withdrawal");
+            }
+            else
+            {
+                Console.WriteLine("Amount must be positive");
+            }
+        }
+        public double Balance
+        { 
+            get { return sum; } 
+        }
+    }
+}
---
+++ Hillel_HomeWorks/14.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _14._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account heikkisAccount = new Account("Heikki", 6584);
+            Account joansAccount = new Account("Joan", 69784);
+            Account lisasAccount = new Account("Lisa", 64);
+            Account mandysAccount = new Account("Mandy", 123);
+            Account danilAccount = new Account("Danil", 14845);
+
+
+            heikkisAccount.WithdrawMoney(420);
+            Console.WriteLine("The balance of Heikki's account is now: " + heikkisAccount.Balance + "$");
+            Console.WriteLine();
+
+            joansAccount.WithdrawMoney(69784);
+            Console.WriteLine("The balance of Joan's account is now: " + joansAccount.Balance + "$");
+            Console.WriteLine();
+
+            lisasAccount.PutMoney(123);
+            Console.WriteLine("The balance of Lisa's account is now: " + lisasAccount.Balance + "$");
+            Console.WriteLine();
+
+            mandysAccount.WithdrawMoney(125);
+            Console.WriteLine("The balance of Mandy's account is now: " + mandysAccount.Balance + "$");
+            Console.WriteLine();
+
+            danilAccount.PutMoney(-589);
+            Console.WriteLine("The balance of Danil's account is now: " + danilAccount.Balance + "$");
+            Console.WriteLine();
+
+        }
+    }
+}
