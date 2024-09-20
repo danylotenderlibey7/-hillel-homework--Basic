2034fdefc23423f2128fcf3e92eed6026981c460
danylo <lysukdanil678@gmail.com>
2024-09-20 22:56:24 +0300

    Added 16.1 Homework!!!

---
+++ Hillel_HomeWorks/16.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace _16._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file1_path = @"D:\Робочий стіл\Hillel\Homeworks\Hillel_HomeWorks\16.1\File 1.txt"; /*"File 1.txt*/
+            string file2_path = @"D:\Робочий стіл\Hillel\Homeworks\Hillel_HomeWorks\16.1\File 2.txt"; /*"File 2.txt*/
+            string content;
+            using (StreamReader reader = new StreamReader(file1_path))
+            {
+                content = reader.ReadToEnd();
+                //Console.WriteLine(content);
+            }
+            using (StreamWriter writer = new StreamWriter(file2_path))
+            {
+                writer.Write(content);  
+            }
+        }
+    }
+}
