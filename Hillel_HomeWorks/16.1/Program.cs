ebb4f00333ee300603d252208885a6a371b82752
danylo <lysukdanil678@gmail.com>
2024-09-21 22:23:43 +0300

    Added entering path of file 16.1

--- Hillel_HomeWorks/16.1/Program.cs
+++ Hillel_HomeWorks/16.1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace _16._1
 {
@@ -7,18 +8,21 @@ namespace _16._1
     {
         static void Main(string[] args)
         {
-            string file1_path = @"D:\Робочий стіл\Hillel\Homeworks\Hillel_HomeWorks\16.1\File 1.txt"; /*"File 1.txt*/
-            string file2_path = @"D:\Робочий стіл\Hillel\Homeworks\Hillel_HomeWorks\16.1\File 2.txt"; /*"File 2.txt*/
+            Console.Write("Enter the path of the first file:");
+            string file1_path = Console.ReadLine();//@"D:\Робочий стіл\Hillel\Homeworks\Hillel_HomeWorks\16.1\File 1.txt"; 
+            Console.Write("Enter the path of the second file:");
+            string file2_path = Console.ReadLine();//@"D:\Робочий стіл\Hillel\Homeworks\Hillel_HomeWorks\16.1\File 2.txt";
             string content;
-            using (StreamReader reader = new StreamReader(file1_path))
+            using (StreamReader reader = new StreamReader(file1_path, Encoding.UTF8))
             {
                 content = reader.ReadToEnd();
                 //Console.WriteLine(content);
             }
-            using (StreamWriter writer = new StreamWriter(file2_path))
+            using (StreamWriter writer = new StreamWriter(file2_path,false, Encoding.UTF8))
             {
                 writer.Write(content);  
             }
+            Console.WriteLine("File has been copied successfully.");
         }
     }
 }
